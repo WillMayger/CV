26e25bf5ebdce03fc2a76a57be1454e6053a2c4a
William Mayger <william.mayger@gforces.co.uk>
2017-09-29 16:15:51 +0100

    building icons into skills component

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 export class Skill extends Component {
   render() {
     return (
-      <div>
-        <div>{this.props.name}</div>
-        <img src={"/images/icon/" + this.props.icon} />
+      <div className="skill">
+        <img alt={this.props.name} title={this.props.name} src={"/images/icon/" + this.props.icon} />
       </div>
     );
   }
