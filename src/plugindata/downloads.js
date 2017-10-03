4736c0152818f99df51b6676c9a70434cf04a354
William Mayger <william.mayger@gforces.co.uk>
2017-10-03 16:01:48 +0100

    added jpg files and pdf, changed downloads so it is now downalodable.

--- src/plugindata/downloads.js
+++ src/plugindata/downloads.js
@@ -1,5 +1,15 @@
 const downloads = [
   {
+    img: 'jpgimg.png',
+    name: 'creative_cv.jpg',
+    link: '/docs/creative_cv.jpg',
+  },
+  {
+    img: 'pdfimg.png',
+    name: 'creative_cv.pdf',
+    link: '/docs/creative_cv.pdf',
+  },
+  {
     img: 'docimg.png',
     name: 'cv.docx',
     link: '/docs/cv.docx',
